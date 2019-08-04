e59618b35967076049a2c8a56f161b771fea887b
Stellar <Stellar@DESKTOP-DPUKMVC>
2019-08-04 17:49:48 +0800

    Add project files.

---
+++ Calculator(WPF)/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator_WPF_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double num1 = Convert.ToDouble(textBox1.Text);
+            double num2 = Convert.ToDouble(textBox2.Text);
+            double result = num1 + num2;
+            label1.Content = result;
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            double num1 = Convert.ToDouble(textBox1.Text);
+            double num2 = Convert.ToDouble(textBox2.Text);
+            double result = num1 - num2;
+            label1.Content = result;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            double num1 = Convert.ToDouble(textBox1.Text);
+            double num2 = Convert.ToDouble(textBox2.Text);
+            double result = num1 * num2;
+            label1.Content = result;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            double num1 = Convert.ToDouble(textBox1.Text);
+            double num2 = Convert.ToDouble(textBox2.Text);
+            double result = num1 / num2;
+            label1.Content = result;
+        }
+    }
+}
